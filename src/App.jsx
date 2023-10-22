1da4d5cadf0e6c69f96a0cced63483a22e62a751
012kirby210 <012kirby210@gmail.com>
2023-10-22 16:52:20 +0200

    Gestion de l'ordre de la liste.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,48 @@ import './App.css'
 import initialData from './initial-data.js';
 import Column from './Column.jsx';
 import { DragDropContext } from '@hello-pangea/dnd';
+import {useState} from "react";
 
 function App() {
 
-  const state = initialData;
+  const [state, setState] = useState(initialData)
 
   const handleDragEnd = (result) => {
+    /**
+     * result = { draggableId, type, reason, source : {droppableid, index }, destination: { droppable, index } }
+     */
+    const { destination, source, draggableId } = result;
+
+    if (!destination){
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ){
+      return ;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskids);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskids: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      }
+    };
+
+    setState(newState);
 
   }
 
